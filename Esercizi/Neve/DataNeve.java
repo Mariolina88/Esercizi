9cbc801179c2e00827bf3275cbcac5042e207ab8
Marialaura Bancheri <maryban@hotmail.it>
2014-04-22 12:18:16 +0200

    New version

--- Esercizi/Neve/DataNeve.java
+++ Esercizi/Neve/DataNeve.java
@@ -1,56 +1,112 @@
 package Neve;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
 
 import ElaborateFile.TextIO;
 import au.com.bytecode.opencsv.CSVReader;
-import au.com.bytecode.opencsv.CSVWriter;
+// TODO: Auto-generated Javadoc
+/**
+ * gets a dataframe from a csv file.  
+ * @author Marialaura Bancheri
+ */
 
 public class DataNeve {
 	
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	public static void main(String[] args) throws IOException {
-		String inputFileName="/Users/marialaura/Desktop/dottorato/Neve/meteo_fin.csv";
+		/**
+		 * The file to be read
+		 */
+		
+		String path;
+		System.out.println("Please enter the path of the input file: ");
+		path = TextIO.getlnString();
+		String fileName;
+		System.out.println("Please enter the name of the input file: ");
+		fileName = TextIO.getlnString();
+		String inputFileName=path+fileName;
+		System.out.println("Check the entire input path:"+inputFileName);
+		
+		//String inputFileName="/Users/marialaura/Desktop/dottorato/Neve/meteo_fin.csv";
 		FileReader f=new FileReader(inputFileName);
 		CSVReader reader = new CSVReader(f);
 		
-		String outputFileName="/Users/marialaura/Desktop/dottorato/Neve/output_prova.csv";
-	    FileWriter newf= new FileWriter(outputFileName);
-	    //CSVWriter writer = new CSVWriter(newf, '\n');
-	    	    
-		String [] nextLine;
-		int cols=2616;
-		int rows=4075;
+		/**
+		 * This first part is necessary to obtain the dimensions of the csv file
+		 * dataRow is the single string of the file read by the CSVReader 
+		 * cols and rows are the dimensions of the dataframe
+		 * vls is the matrix of cols*rows dimensions in which will be stored 
+		 * the values of the dataframe 
+		 * 
+		 */
+	    
+		String[] dataRow = reader.readNext();
+		int rows=0;
+		int cols=dataRow.length;;
+		while (dataRow != null){
+	          rows++;
+	          dataRow = reader.readNext();
+	        }
+		reader.close();
+		
+		/**
+		 * In this second part we must apply again the class CSVreader
+		 * in order to read again the csv file from the first line
+		 * vls is the matrix of cols*rows dimensions in which will be stored 
+		 * the values of the dataframe 
+		 * 
+		 */
+		
+		
+		FileReader fnew=new FileReader(inputFileName);
+		CSVReader readernew = new CSVReader(fnew);
+		
 		String [][] vls=new String [rows][cols];
-
+		String [] nextLine;
 			for(int i=0;i<rows;i++){
-				nextLine = reader.readNext();
+				nextLine = readernew.readNext();
 				for(int k=0;k<cols;k++){
 	        	vls[i][k]=nextLine[k];
 	        	//System.out.print(vls[i][k]+" ");
 				}
 				//System.out.println("");
 			} 
-	    
-		
-			reader.close();	
 
+			reader.close();
+			
+			/**
+			 * write the Data frame to a specified file (outputFileName)
+			 * in a specified format (in this case a csv file)
+			 * 
+			 */
+			
+			String outputName;
+			System.out.println("Please enter the name of the output file: ");
+			outputName = TextIO.getlnString();
+			String outputFileName=path+outputName;
+			System.out.println("Check the entire output path:"+outputFileName);
+			//String outputFileName="/Users/marialaura/Desktop/dottorato/Neve/output_prova.csv";
 			TextIO.writeFile(outputFileName);
+			
+			String typeSeparator;
+			System.out.println("Please enter the type of Separator:");
+			typeSeparator = TextIO.getlnString();
+			
 
-			//writes data
 			for(int i=0;i<rows;i++){
 				for(int j=0;j<2;j++){
-					TextIO.putf(vls[i][j]+",");
+					TextIO.putf(vls[i][j]+typeSeparator);
 				}
 				TextIO.putf("\n");
 			}
-		}
-		
-	    
+		}    
 	}
 
 
